42f8af72d5ab0a845c4792976d24971b4f402f18
marckent04 <marchenrynanguy1@gmail.com>
2025-04-21 12:06:20 +0200

    feat(navigation): add  step for  navigate to the previous page

---
+++ internal/steps_definitions/frontend/navigation/i_navigate_back.go
@@ -0,0 +1,25 @@
+package navigation
+
+import (
+	"testflowkit/internal/steps_definitions/core"
+	"testflowkit/shared"
+)
+
+func (n navigation) iNavigateBack() core.TestStep {
+	return core.NewStepWithoutVariables(
+		[]string{"I navigate back"},
+		func(ctx *core.TestSuiteContext) func() error {
+			return func() error {
+				ctx.GetCurrentPage().Back()
+				return nil
+			}
+		},
+		nil,
+		core.StepDefDocParams{
+			Description: "navigates back to the previous page.",
+			Variables:   nil,
+			Example:     "Given I navigate back",
+			Category:    shared.Navigation,
+		},
+	)
+}
