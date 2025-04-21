1e0f5fb205cf6a5f7770004c8e463aa061e5ea00
marckent04 <marchenrynanguy1@gmail.com>
2025-04-21 12:06:40 +0200

    feat(navigation): add step for wait a specified number of seconds

---
+++ internal/steps_definitions/frontend/navigation/i_wait_x_seconds.go
@@ -0,0 +1,30 @@
+package navigation
+
+import (
+	"testflowkit/internal/steps_definitions/core"
+	"testflowkit/shared"
+	"time"
+)
+
+func (n navigation) iWait() core.TestStep {
+	testDefinition := func(_ *core.TestSuiteContext) func(int) error {
+		return func(seconds int) error {
+			time.Sleep(time.Duration(seconds) * time.Second)
+			return nil
+		}
+	}
+
+	return core.NewStepWithOneVariable[int](
+		[]string{`^I wait for {int} seconds$`},
+		testDefinition,
+		nil,
+		core.StepDefDocParams{
+			Description: "Waits for a specified number of seconds.",
+			Variables: []shared.StepVariable{
+				{Name: "seconds", Description: "The number of seconds to wait for.", Type: shared.DocVarTypeInt},
+			},
+			Example:  "When I wait for 5 seconds",
+			Category: shared.Navigation,
+		},
+	)
+}
