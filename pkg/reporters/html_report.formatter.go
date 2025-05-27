235667345caaddbb1106b7e6f319db04c5a15bb1
marckent04 <marchenrynanguy1@gmail.com>
2025-05-27 13:59:12 +0200

    refactor(reporters): enhance HTML report formatting and structure

--- pkg/reporters/html_report.formatter.go
+++ pkg/reporters/html_report.formatter.go
@@ -2,6 +2,7 @@ package reporters
 
 import (
 	_ "embed"
+	"fmt"
 	"html/template"
 	"log"
 	"os"
@@ -14,13 +15,11 @@ var reportTemplate string
 
 type htmlReportFormatter struct{}
 
-func (r htmlReportFormatter) format(ts testSuiteDetails) string {
+func (r htmlReportFormatter) format(ts htmlTestSuiteDetails) string {
 	tmpl, err := template.New("report").Parse(reportTemplate)
 	if err != nil {
 		logger.Fatal("cannot parse report template", err)
 	}
-
-	ts.resume()
 
 	wr := utils.TextWriter{}
 	err = tmpl.Execute(&wr, ts)
@@ -32,8 +31,7 @@ func (r htmlReportFormatter) format(ts testSuiteDetails) string {
 }
 
 func (r htmlReportFormatter) WriteReport(details testSuiteDetails) {
-	content := r.format(details)
-
+	content := r.formatContent(details)
 	file, err := os.Create("report.html")
 	if err != nil {
 		log.Panicf("cannot create reporters file in this folder ( %s )\n", err)
@@ -43,5 +41,45 @@ func (r htmlReportFormatter) WriteReport(details testSuiteDetails) {
 	_, err = file.WriteString(content)
 	if err != nil {
 		log.Panicf("error when reporters filling ( %s )", err)
+	}
+}
+
+func (r htmlReportFormatter) formatContent(details testSuiteDetails) string {
+	htmlScenarios := make([]htmlScenario, len(details.Scenarios))
+	for i, sc := range details.Scenarios {
+		htmlScenarios[i] = *newhtmlScenario(sc)
+	}
+
+	return r.format(htmlTestSuiteDetails{
+		testSuiteDetails: details,
+		Scenarios:        htmlScenarios,
+	})
+}
+
+type htmlScenario struct {
+	Scenario
+	FmtDuration          string
+	HTMLStatusColorClass string
+}
+
+type htmlTestSuiteDetails struct {
+	testSuiteDetails
+	Scenarios []htmlScenario
+}
+
+func newhtmlScenario(sc Scenario) *htmlScenario {
+	color := "red"
+	if sc.Result == succeeded {
+		color = "green"
+	}
+
+	if sc.ErrorMsg == "" {
+		sc.ErrorMsg = "-"
+	}
+
+	return &htmlScenario{
+		Scenario:             sc,
+		FmtDuration:          sc.Duration.String(),
+		HTMLStatusColorClass: fmt.Sprintf("bg-%s-500", color),
 	}
 }
