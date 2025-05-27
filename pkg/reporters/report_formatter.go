3880366396b6f2a515eef4fbcee41150f06703f8
marckent04 <marchenrynanguy1@gmail.com>
2025-05-27 19:05:32 +0200

    feat(reporters): implement JSON report generation

--- pkg/reporters/report_formatter.go
+++ pkg/reporters/report_formatter.go
@@ -1,10 +1,7 @@
 package reporters
 
 import (
-	"fmt"
 	"log"
-	"strconv"
-	"time"
 )
 
 type formatter interface {
@@ -17,62 +14,4 @@ type disabledFormatter struct {
 func (f disabledFormatter) WriteReport(details testSuiteDetails) {
 	const sentence = "%d tests executed successfully at %s"
 	log.Printf(sentence, len(details.Scenarios), details.StartDate)
-}
-
-type testSuiteDetails struct {
-	AppName, AppVersion string
-	ExecutionDate       string
-	TotalExecutionTime  string
-	TotalTests          string
-	SucceededTests      string
-	FailedTests         string
-	SuccessRate         string
-	StartDate           time.Time
-	Scenarios           []Scenario
-}
-
-func (ts *testSuiteDetails) getTestSuiteDurationInSeconds() int {
-	var total time.Duration
-	for _, sc := range ts.Scenarios {
-		total += sc.Duration
-	}
-	return int(total.Seconds())
-}
-
-func (ts *testSuiteDetails) getScenarioResults() (int, int) {
-	var succeedSc, failedSc int
-	for _, sc := range ts.Scenarios {
-		if sc.Result == succeeded {
-			succeedSc++
-		} else {
-			failedSc++
-		}
-	}
-	return succeedSc, failedSc
-}
-
-func newTestSuiteDetails(
-	appName, appVersion string, startDate time.Time, scenarios []Scenario,
-) *testSuiteDetails {
-	ts := testSuiteDetails{
-		AppName:    appName,
-		AppVersion: appVersion,
-		StartDate:  startDate,
-		Scenarios:  scenarios,
-	}
-
-	year, month, day := ts.StartDate.Date()
-	dateTime := fmt.Sprintf("%d-%d-%d at %d:%d", month, day, year, ts.StartDate.Hour(), ts.StartDate.Minute())
-
-	total := len(ts.Scenarios)
-	succeedSc, failedSc := ts.getScenarioResults()
-
-	ts.TotalTests = strconv.Itoa(total)
-	ts.SucceededTests = strconv.Itoa(succeedSc)
-	ts.FailedTests = strconv.Itoa(failedSc)
-	ts.ExecutionDate = dateTime
-	ts.TotalExecutionTime = fmt.Sprintf("%ds", ts.getTestSuiteDurationInSeconds())
-	ts.SuccessRate = strconv.Itoa(succeedSc * 100 / total)
-
-	return &ts
 }
