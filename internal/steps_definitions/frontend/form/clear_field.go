46e1ba91fa46bd78b898e1a85facad4a1a9b2cd7
marckent04 <marchenrynanguy1@gmail.com>
2025-06-04 21:35:18 +0200

    feat: add clear field step definition

---
+++ internal/steps_definitions/frontend/form/clear_field.go
@@ -0,0 +1,45 @@
+package form
+
+import (
+	"testflowkit/internal/browser"
+	"testflowkit/internal/config/testsconfig"
+	"testflowkit/internal/steps_definitions/core"
+	"testflowkit/internal/utils/stringutils"
+	"testflowkit/shared"
+)
+
+func (s steps) userClearsFormField() core.TestStep {
+	formatLabel := func(label string) string {
+		return stringutils.SuffixWithUnderscore(label, "field")
+	}
+
+	return core.NewStepWithOneVariable(
+		[]string{`^the user clears the {string} field.`},
+		func(ctx *core.TestSuiteContext) func(string) error {
+			return func(inputLabel string) error {
+				input, err := browser.GetElementByLabel(ctx.GetCurrentPage(), formatLabel(inputLabel))
+				if err != nil {
+					return err
+				}
+				return input.Clear()
+			}
+		},
+		func(inputLabel string) core.ValidationErrors {
+			vc := core.ValidationErrors{}
+			label := formatLabel(inputLabel)
+			if !testsconfig.IsElementDefined(label) {
+				vc.AddMissingElement(label)
+			}
+
+			return vc
+		},
+		core.StepDefDocParams{
+			Description: "Removes any existing text or value from an input field identified by its logical name.",
+			Variables: []shared.StepVariable{
+				{Name: "inputLabel", Description: "The label of the input.", Type: shared.DocVarTypeString},
+			},
+			Example:  `When the user clears the "Search" field.`,
+			Category: shared.Form,
+		},
+	)
+}
